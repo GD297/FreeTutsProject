9f04f915674f3fa76d63142882e5001508e44f6a
0112312414 <pdcthanh112@gmail.com>
2021-02-23 13:14:25 +0700

    asdf

---
+++ FreeTutsProject/src/main/java/fsoft/group1/FreeTutsProjectApplication.java
@@ -0,0 +1,13 @@
+package fsoft.group1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeTutsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeTutsProjectApplication.class, args);
+	}
+
+}
---
+++ FreeTutsProject/src/test/java/fsoft/group1/FreeTutsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package fsoft.group1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeTutsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
